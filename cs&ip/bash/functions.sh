251411cdc1a51e83f167b17828fe855cee138529
uluk001 <ulukmanmuratov@gmail.com>
2025-10-21 19:57:54 +0600

    fix: quote variables to prevent empty input bug in secret check

--- cs&ip/bash/functions.sh
+++ cs&ip/bash/functions.sh
@@ -1,21 +1,20 @@
 #! /usr/bin/env bash
 # Get access to the secret link
 secret_word="snickers"
-link=https://youtu.be/QtXby3twMmI?si=34W9pvRZDUQXV5xO
+link="https://youtu.be/QtXby3twMmI?si=34W9pvRZDUQXV5xO"
 
 check_secret_with__retry() {
-	echo "Write the secret word: "
-	read user_input
-	while [ $1 != $user_input ]; do
-	        if [ $1 != $user_input ]; then
-	                echo "Secret is not correct, try one more time:"
-	                read user_input
-	        fi
-	done
-	local result=$1=$user_input
-	echo $result
+    local expected="$1"
+    echo "Write the secret word: "
+    read -r user_input
+    while [ "$expected" != "$user_input" ]; do
+        echo "Secret is not correct, try one more time:"
+        read -r user_input
+    done
+    local result="$user_input"
+    echo "$result"
 }
 
-(check_secret_with__retry $secret_word)
+check_secret_with__retry "$secret_word"
 
 echo "Oh! Great, you guessed the word, enjoy: $link"
