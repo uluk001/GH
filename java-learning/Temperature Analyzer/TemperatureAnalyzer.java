1a2368b8c42d3c64fdd9b9785c9b1bb29b677c71
uluk001 <ulukmanmuratov@gmail.com>
2026-01-07 18:02:22 +0600

    Refactor TemperatureAnalyzer: optimize calculations for max temperature and days above average, replacing ArrayList with integer count.

--- java-learning/Temperature Analyzer/TemperatureAnalyzer.java
+++ java-learning/Temperature Analyzer/TemperatureAnalyzer.java
@@ -5,15 +5,13 @@
 // Сколько дней было выше среднего
 // Применяет: массивы, циклы, методы
 
-import java.util.ArrayList;
-
 public class TemperatureAnalyzer {
     public static void main(String[] args) {
         double[] weekTemperatures = {32.4, 35.2, 28.0, 25.9, 30.3, 31.4, 33.0};
         double sumTemp = 0;
-        double maxTemp = 0;
+        double maxTemp = weekTemperatures[0];
         double minTemp = weekTemperatures[0];
-        ArrayList<Double> greaterThanMean = new ArrayList<>();
+        int greaterThanMean = 0;
 
         for (double dayTemp : weekTemperatures) {
             if (dayTemp > maxTemp) {
@@ -27,13 +25,13 @@ public class TemperatureAnalyzer {
 
         for (double dayTemp : weekTemperatures) {
             if (dayTemp > sumTemp / weekTemperatures.length) {
-                greaterThanMean.add(dayTemp);
+                greaterThanMean += 1;
             }
         }
 
         System.out.printf("Min temp for a week is: %s\n", minTemp);
         System.out.printf("Max temp for a week is: %s\n\n", maxTemp);
         System.out.printf("Mean temp for a week is: %s\n", sumTemp / weekTemperatures.length);
-        System.out.printf("How many days were above average? %d\n", greaterThanMean.size());
+        System.out.printf("How many days were above average? %d\n", greaterThanMean);
     }
 }
