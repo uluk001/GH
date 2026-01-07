3bdfc359b8c192e5cce66d8ed8835a39f37aff12
uluk001 <ulukmanmuratov@gmail.com>
2026-01-07 17:41:34 +0600

    Add TemperatureAnalyzer class: implemented functionality to analyze weekly temperatures, including calculations for min, max, mean, and days above average.

---
+++ java-learning/Temperature Analyzer/TemperatureAnalyzer.java
@@ -0,0 +1,39 @@
+// Temperature Analyzer
+
+// Массив температур за неделю
+// Найти среднюю, максимальную, минимальную
+// Сколько дней было выше среднего
+// Применяет: массивы, циклы, методы
+
+import java.util.ArrayList;
+
+public class TemperatureAnalyzer {
+    public static void main(String[] args) {
+        double[] weekTemperatures = {32.4, 35.2, 28.0, 25.9, 30.3, 31.4, 33.0};
+        double sumTemp = 0;
+        double maxTemp = 0;
+        double minTemp = weekTemperatures[0];
+        ArrayList<Double> greaterThanMean = new ArrayList<>();
+
+        for (double dayTemp : weekTemperatures) {
+            if (dayTemp > maxTemp) {
+                maxTemp = dayTemp;
+            }
+            if (dayTemp < minTemp) {
+                minTemp = dayTemp;
+            }
+            sumTemp += dayTemp;
+        }
+
+        for (double dayTemp : weekTemperatures) {
+            if (dayTemp > sumTemp / weekTemperatures.length) {
+                greaterThanMean.add(dayTemp);
+            }
+        }
+
+        System.out.printf("Min temp for a week is: %s\n", minTemp);
+        System.out.printf("Max temp for a week is: %s\n\n", maxTemp);
+        System.out.printf("Mean temp for a week is: %s\n", sumTemp / weekTemperatures.length);
+        System.out.printf("How many days were above average? %d\n", greaterThanMean.size());
+    }
+}
